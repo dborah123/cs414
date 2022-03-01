61ae4843e922fec6e469f129f85d8ade3a284ac5
Dan Borah <dborah@mydomain.myhome.home>
2022-02-28 22:23:58 -0500

    working strace+notes

--- assignment1/mystrace.d
+++ assignment1/mystrace.d
@@ -2,10 +2,12 @@
  * mystrace.d
  */
 
+/* ENTRY PROBES */
+
 syscall:::entry
-/arg5 != 0 && arg4 !=0/
+/arg5 != 0 && arg4 !=0 && pid == $1/
 {
-	printf("%s(%d, %d, %d, %d, %d, %d)\n", 
+	printf("%s(%d, %d, %d, %d, %d, %d)\t\t", 
 			probefunc, 
 			arg0, 
 			arg1, 
@@ -17,47 +19,49 @@ syscall:::entry
 }
 
 syscall:::entry
-/arg4 != 0 && arg5 == 0/
+/arg4 != 0 && arg5 == 0 && pid == $1/
 {
-	printf("%s(%d, %d, %d, %d, %d)\n", 
-			probefunc, 
-			arg0, 
-			arg1, 
-			arg2, 
-			arg3, 
-			arg4
-		);
+	printf("%s(%d, %d, %d, %d, %d)\t\t", probefunc, arg0, arg1, arg2, arg3, arg4);
 
 }
 
 syscall:::entry
-/arg2 != 0 && arg3 == 0/
+/arg3 != 0 && arg4 == 0 && pid == $1/
 {
-	printf("%s(%d, %d, %d)\n", probefunc, arg0, arg1, arg2);
+	printf("%s(%d, %d, %d, %d)\t\t", probefunc, arg0, arg1, arg2, arg3);
+
 }
 
 syscall:::entry
-/arg1 != 0 && arg3 == 0/
+/arg2 != 0 && arg3 == 0 && pid == $1/
 {
-	printf("%s(%d, %d)\t", probefunc, arg0, arg1);
+	printf("%s(%d, %d, %d)\t\t", probefunc, arg0, arg1, arg2);
 }
 
 syscall:::entry
-/arg3 != 0 && arg4 == 0/
+/arg1 != 0 && arg2 == 0 && pid == $1/
 {
-	printf("%s(%d, %d, %d, %d)\n", 
-			probefunc, 
-			arg0, 
-			arg1, 
-			arg2, 
-			arg3
-		);
-
+	printf("%s(%d, %d)\t\t", probefunc, arg0, arg1);
 }
 
 
 syscall:::entry
-/arg0 != 0 && arg1 == 0/
+/arg0 != 0 && arg1 == 0 && pid == $1/
 {
-	printf("%s(%d)\n", probefunc, arg0);
+	printf("%s(%d)\t\t", probefunc, arg0);
 }
+
+syscall:::entry
+/arg0 == 0 && arg1 == 0 && pid == $1/
+{
+	printf("%s()\t\t", probefunc);
+}
+
+
+/* RETURN PROBES */
+syscall:::return
+/pid == $1/
+{
+	printf("\t= %d\n", arg1);
+}
+
