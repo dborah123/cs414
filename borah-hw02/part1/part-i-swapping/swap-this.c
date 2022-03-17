2430f05a6dc4cff4a895efe85b80d44d00ac3608
Dan Borah <danielborah@gmail.com>
2022-03-17 00:03:36 -0400

    more work... section 1 part 6 ugh

--- borah-hw02/part1/part-i-swapping/swap-this.c
+++ borah-hw02/part1/part-i-swapping/swap-this.c
@@ -6,51 +6,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <malloc_np.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <limits.h>
 
 int
 main(int argc, char *argv[])
 {
+/*
+    char *q = malloc(5);
+    *q = 'g';
+    for (int j = 0; j < INT_MAX; j++) {
+        char *p = malloc(10000 * 10000);
+
+        for (int i = 0; i < 10000; i++) {
+            p[i * 100000] = 0;
+        }
+    }
+    *q = 'a';
     pid_t mypid;
     mypid = getpid();
-    printf("my pid is %d\n", mypid);
+    kill(mypid, SIGKILL);
+*/
 
-    char vmcmd[32];
-    snprintf(vmcmd, 32, "procstat vm %d", mypid);
+/*
+    char *p = mmap(NULL, INT_MAX, PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    *p = 'a';
 
-    printf("Before mmap\n");
-    system(vmcmd);
+    char *q = mmap(NULL, INT_MAX, PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    *q = 'b';
 
+    *p = 'c';
+*/
+/*
+    char *d = mmap(NULL, INT_MAX, PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    *d = 'a';
+    printf("here\n");
     char *p;
+    for (int i = 0; i < INT_MAX; i++) {
 
-    for (int i = 0; i < 20970; i++) {
-
-        p = mmap(NULL, 100000, PROT_NONE, MAP_ANON | MAP_PRIVATE | MAP_32BIT, -1, 0);
+        p = mmap(NULL, INT_MAX, PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
 
         if (p == MAP_FAILED) {
             perror("mmap");
             exit(1);
         }
+	*p = 'a';
     }
 
-    printf("After mmap:\n");
-    system(vmcmd);
-    
-    printf("One more mmap but without `MAP_32BIT`\n");
-    p = mmap(NULL, 100000, PROT_NONE, MAP_ANON | MAP_PRIVATE, -1, 0);
-
-    if (p == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }
-	
-    system(vmcmd);
- 
-    printf("One more allocation in phys mem to show that physical memory is full\n");
-    p = mmap(NULL, 100000, PROT_NONE, MAP_ANON | MAP_PRIVATE | MAP_32BIT, -1, 0);
-
-    if (p == MAP_FAILED) {
-        perror("mmap");
-        exit(1);
-    }	
+    free(d);
+*/
 }
-
